5d7304c0d6173613866bc41a042c665fc3c187b0
generousdev <omolaja.abubakar05@gmail.com>
2022-05-30 11:21:10 +0100

    Adding Configuration

---
+++ Desktop/demo-cypress/cypress/support/step_definitions/login.js
@@ -0,0 +1,47 @@
+import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
+
+import LoginPage from '../../integration/pages/LoginPage';
+
+import SecurePage from '../../integration/pages/SecurePage';
+
+const loginPage = new LoginPage();
+
+const securePage = new SecurePage();
+
+ 
+
+Given('I open LoginPage', () => {
+
+  loginPage.visitLogin();
+
+});
+
+When(`I fill the username input with ${string}`, username => {
+  loginPage.typeUsername(username);
+});
+
+ 
+
+When(`I fill the password input with ${string}`, password => {
+  loginPage.typePassword(password);
+});
+
+ 
+
+When('I click on Login button', () => {
+  loginPage.clickLogin();
+});
+
+ 
+
+Then(`I see ${string} message`, expected => {
+
+  const message = securePage.getMessage();
+
+  message.should((actual) => {
+
+    expect(actual).to.have.string(expected);
+
+  })
+
+});
